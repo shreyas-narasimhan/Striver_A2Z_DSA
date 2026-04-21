893d3e24982429c595edcacd6f9dcd4ead8f45be
Shreyas Narasimhan <shreyas.narasimhan2000@gmail.com>
2026-04-21 19:17:51 +0530

    completed a problem on arrays

---
+++ Problems on Arrays/Easy/Linear Search in C.java
@@ -0,0 +1,29 @@
+import java.util.*;
+public class LinearSearchInC {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Enter the number of elements in the array: ");
+        int n = sc.nextInt();
+        int[] arr = new int[n];
+        System.out.println("Enter the elements of the array:");
+        for (int i = 0; i < n; i++) {
+            arr[i] = sc.nextInt();
+        }
+        System.out.print("Enter the element to search: ");
+        int key = sc.nextInt();
+        int result = linearSearch(arr, key);
+        if (result == -1) {
+            System.out.println("Element not found in the array.");
+        } else {
+            System.out.println("Element found at index: " + result);
+        }
+    }
+    public static int linearSearch(int[] arr, int key) {
+        for (int i = 0; i < arr.length; i++) {
+            if (arr[i] == key) {
+                return i; // Return the index of the found element
+            }
+        }
+        return -1; // Return -1 if the element is not found
+    }
+}
